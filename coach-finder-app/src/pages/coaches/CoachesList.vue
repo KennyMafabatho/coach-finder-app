f83bbb257f03c03a2215d3c75b043a655fa98ef4
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-04-22 18:00:34 +0200

    Registered store and displayed coaches

--- coach-finder-app/src/pages/coaches/CoachesList.vue
+++ coach-finder-app/src/pages/coaches/CoachesList.vue
@@ -1,9 +1,31 @@
 <template>
+<div>
+  <section> FILTER</section>
   <section>
-    FILTER
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+        <li v-for="coach in filteredCoaches" :key="coach.id">
+         {{ coach.firstName }}
+        </li>
+    </ul>
+    <h3 v-else>No Coaches Found.</h3>
   </section>
-  <section>
-    LIST OF COACHES
-  </section>
+</div>
 </template>
+
+<script>
+export default {
+  computed:{
+    filteredCoaches(){
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches(){
+      return this.$store.getters['coaches/hasCoaches'];
+    }
+  }
+}
+</script>
 
