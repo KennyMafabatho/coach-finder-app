2b28d462c899b288114ad92c75b5ae7efb317529
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-04-23 11:20:40 +0200

    Added coach to vuex

--- coach-finder-app/src/store/modules/coaches/actions.js
+++ coach-finder-app/src/store/modules/coaches/actions.js
@@ -1,1 +1,13 @@
-export default{};
+export default{
+  registerCoach(context, data){
+    const coachData = {
+      id: 'c3',
+      firstName: data.firstName,
+      lastName: data.lastName,
+      description: data.description,
+      hourlyRate: data.rate,
+      areas: data.areas
+    };
+    context.commit('registerCoach',coachData);
+  }
+};
