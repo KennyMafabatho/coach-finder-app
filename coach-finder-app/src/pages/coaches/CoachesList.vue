6187056fea1bc756b3300e2d4f21429e2bc7331c
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-04-23 02:26:37 +0200

    Registered base components and added their styles

--- coach-finder-app/src/pages/coaches/CoachesList.vue
+++ coach-finder-app/src/pages/coaches/CoachesList.vue
@@ -2,22 +2,24 @@
 <div>
   <section> FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-        <coach-item 
-          v-for="coach in filteredCoaches" 
-          :key="coach.id" 
-          :id="coach.id" 
-          :first-name="coach.firstName"
-          :last-name="coach.lastName"
-          :rate="coach.hourlyRate"
-          :areas="coach.areas"
-        ></coach-item>
-    </ul>
-    <h3 v-else>No Coaches Found.</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+          <coach-item 
+            v-for="coach in filteredCoaches" 
+            :key="coach.id" 
+            :id="coach.id" 
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+          ></coach-item>
+      </ul>
+      <h3 v-else>No Coaches Found.</h3>
+    </base-card>
   </section>
 </div>
 </template>
@@ -25,10 +27,12 @@
 <script>
 
 import CoachItem from '../../components/coaches/CoachItem.vue';
+import BaseButton from '../../components/ui/BaseButton.vue';
 
 export default {
   components: { 
-    CoachItem 
+    CoachItem,
+    BaseButton 
   },
   computed:{
     filteredCoaches(){
