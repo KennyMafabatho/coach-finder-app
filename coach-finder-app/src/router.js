8c84d24ac9b70aa612156329da86d3b794883df0
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-04-29 11:38:32 +0200

    Implemented navigation guards

--- coach-finder-app/src/router.js
+++ coach-finder-app/src/router.js
@@ -7,6 +7,7 @@ import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
+import store from './store/index.js';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -32,15 +33,18 @@ const router = createRouter({
     },
     {
       path: '/register',
-      component: CoachRegistration
+      component: CoachRegistration,
+      meta:{ requiresAuth : true}
     },
     {
       path:'/requests', 
-      component: RequestsReceived
+      component: RequestsReceived,
+      meta: { requiresAuth: true }
     },
     {
       path: '/auth',
-      component: UserAuth
+      component: UserAuth,
+      meta:{ requiresUnauth: true}
     },
     {
       path: '/:notFound(.*)',
@@ -49,5 +53,15 @@ const router = createRouter({
 
   ]
 });
+
+router.beforeEach(function(to,_, next){
+  if(to.meta.requiresAuth && store.getters.isAuthenticated){
+    next('/auth');
+  }else if(to.meta.requiresUnauth && store.getters.isAuthenticated){
+    next('/coaches')
+  }else{
+    next();
+  }
+})
 
 export default router;
